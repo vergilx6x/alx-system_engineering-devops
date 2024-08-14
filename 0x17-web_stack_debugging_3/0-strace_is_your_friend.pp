9ad9320e141cc6e304d31f70cbb580c6d0db54cd
vergilx6x <med.work96@hotmail.com>
2024-08-14 10:26:17 -0700

    Done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Automated puppet fix (Apache returning a 500 error)
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
